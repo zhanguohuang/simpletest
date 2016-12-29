a3db27e01310d4f6ec13b1bd94e6d55dade47b9c
zhanguohuang <zhanguohuang@szhuarong.com>
2016-12-29 15:35:17 +0800

    web.xml缺少了listener load applicationContext

--- src/main/java/org/service/impl/TestServiceImpl.java
+++ src/main/java/org/service/impl/TestServiceImpl.java
@@ -1,7 +1,9 @@
 package org.service.impl;
 
 import org.service.TestService;
+import org.springframework.stereotype.Service;
 
+@Service("testService")
 public class TestServiceImpl implements TestService {
 
 	public void test() {
